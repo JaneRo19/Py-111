2de494d6e67a5ac3f41567af1b215d9655b64c3c
Jane1930 <vishnyakova.eugenya@gmail.com>
2022-02-19 13:26:49 +0300

    b1_binary_search

--- Tasks/b1_binary_search.py
+++ Tasks/b1_binary_search.py
@@ -9,5 +9,20 @@ def binary_search(elem: int, arr: Sequence) -> Optional[int]:
     :param arr: array where element is to be found
     :return: Index of element if it's presented in the arr, None otherwise
     """
-    print(elem, arr)
-    return None
+    left_border = 0
+    right_border = len(arr)
+
+    while left_border < right_border:
+        middle = (right_border + left_border) // 2
+
+        if elem > arr[middle]:
+            left_border = middle + 1
+        else:
+            right_border = middle
+
+        if arr[left_border] == elem:
+            return left_border
+
+
+
+

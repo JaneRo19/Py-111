985f8f9d9b9bbb7050f62a50b3a9b93f5fc6683f
Jane1930 <vishnyakova.eugenya@gmail.com>
2022-02-21 22:47:59 +0100

    fixed a3_check_brackets

--- Tasks/a3_check_brackets.py
+++ Tasks/a3_check_brackets.py
@@ -6,21 +6,11 @@ def check_brackets(brackets_row: str) -> bool:
     :return: True if valid, False otherwise
     """
 
-    stack = []
-    is_good = True
-    for bracket in brackets_row:
-        if bracket == "(":
-            stack.append(bracket)
-        elif bracket == ")":
-            if not stack:
-                is_good = False
-                break
-            open_bracket = stack.pop()
-            if open_bracket == "(" and ")":
-                continue
-            is_good = False
-    if is_good and len(stack) == 0:
-        return True
-    else:
-        return False
+    counter = 0
+    for i in range(len(brackets_row)):
+        if brackets_row[i] == "(" and brackets_row[0] != ")":
+            counter += 1
+        else:
+            counter -= 1
+    return True if counter == 0 else False
 

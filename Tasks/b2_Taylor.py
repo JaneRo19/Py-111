0d7efc8e46441ea6b805ad483ebb3b7f6cc62813
Jane1930 <vishnyakova.eugenya@gmail.com>
2022-02-24 00:40:25 +0100

    b2_Taylor

--- Tasks/b2_Taylor.py
+++ Tasks/b2_Taylor.py
@@ -2,6 +2,9 @@
 Taylor series
 """
 from typing import Union
+from math import factorial
+from itertools import count
+EPSILON = 0.00001
 
 
 def ex(x: Union[int, float]) -> float:
@@ -11,8 +14,17 @@ def ex(x: Union[int, float]) -> float:
     :param x: x value
     :return: e^x value
     """
-    print(x)
-    return 0
+
+    sum_ = 0
+    top = 1
+    bottom = 1
+    for i in count(1, 1):
+        current_item = top / bottom
+        sum_ += current_item
+        if abs(current_item) < EPSILON:
+            return sum_
+        top = top * x
+        bottom = bottom * i
 
 
 def sinx(x: Union[int, float]) -> float:
@@ -22,5 +34,13 @@ def sinx(x: Union[int, float]) -> float:
     :param x: x value
     :return: sin(x) value
     """
-    print(x)
-    return 0
+    def get_item_n(n):
+        return ((-1) ** (n-1) * x ** (2 * n - 1)) / factorial(2 * n -1)
+
+    sum_ = 0
+    for i in count(1, 1):
+        current_item = get_item_n(i)
+        sum_ += current_item
+        if abs(current_item) < EPSILON:
+            return sum_
+

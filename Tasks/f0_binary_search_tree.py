3166a94cee4837eb77a82ccdda412d7b20962257
Jane1930 <vishnyakova.eugenya@gmail.com>
2022-03-13 09:11:00 +0100

    f0_binary_search_tree_not_release

--- Tasks/f0_binary_search_tree.py
+++ Tasks/f0_binary_search_tree.py
@@ -56,18 +56,64 @@ class BinarySearchTree:
         :param key: key to be removed
         :return: deleted (key, value) pair or None
         """
-        print(key)
-        return None
+        current_node, prev_node = self._find(key)
+        if current_node["key"] is not None:
+            return None
+
+        while True:
+            if key < current_node["key"]:
+                current_node["key"].remove()
+                current_node["key"] = min(current_node["right"])
+            else:
+                current_node["key"].remove()
+                current_node["key"] = max(current_node["left"])
+
+        if current_node["right"] is None and current_node["left"] is None:
+            current_node["key"].remove()
+        else:
+            if current_node["left"] is None:
+                current_node["key"].remove()
+
+            elif current_node["right"] is None:
+                current_node["key"].remove()
+
 
     def find(self, key: int) -> Optional[Any]:
-        """
-        Find value by given key in the BST
 
-        :param key: key for search in the BST
-        :return: value associated with the corresponding key
-        """
-        print(key)
-        return None
+        current_node, _ = self._find(key)
+        if current_node is not None:
+            return current_node["value"]
+
+        # current_node = self.root
+        # current_key = current_node["key"]
+        # while True:
+        #     if key < current_key:
+        #         if current_node["left"] is None:
+        #             raise KeyError
+        #         else:
+        #             current_node = current_node["left"]
+        #
+        #     else:
+        #         if current_node["right"] is None:
+        #             raise KeyError
+        #         else:
+        #             current_node = current_node["right"]
+        #
+        #     return current_node["value"]
+
+    def _find(self, key):
+        prev_node = None
+        current_node = self.root
+        while current_node["key"] is not None:
+            if current_node["key"] == key:
+                break
+
+            prev_node = current_node
+            if current_node["key"] < key:
+                current_node = current_node["right"]
+            else:
+                current_node = current_node["left"]
+        return current_node, prev_node
 
     def clear(self) -> None:
         """
@@ -75,4 +121,4 @@ class BinarySearchTree:
 
         :return: None
         """
-        return None
+        self.root = None

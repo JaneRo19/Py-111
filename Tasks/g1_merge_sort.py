c8e9cb2ac7a47d827ef99676564e6404a17cab93
Jane1930 <vishnyakova.eugenya@gmail.com>
2022-03-13 09:38:04 +0100

    g1_merge_sort

--- Tasks/g1_merge_sort.py
+++ Tasks/g1_merge_sort.py
@@ -8,4 +8,27 @@ def sort(container: List[int]) -> List[int]:
     :param container: container of elements to be sorted
     :return: container sorted in ascending order
     """
-    return container
+    n = len(container)
+    if n < 2:
+        return container
+
+    left = sort(container[:n // 2])
+    right = sort(container[n // 2:])
+    i = j = 0
+    res_container = []
+
+    while i < len(left) or j < len(right):
+        if not i < len(left):
+            res_container.append(right[j])
+            j += 1
+        elif not j < len(right):
+            res_container.append(left[i])
+            i += 1
+        elif left[i] < right[j]:
+            res_container.append(left[i])
+            i += 1
+        else:
+            res_container.append(right[j])
+            j += 1
+
+    return res_container
